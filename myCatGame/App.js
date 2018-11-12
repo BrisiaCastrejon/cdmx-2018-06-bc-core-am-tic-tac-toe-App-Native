4958ddc483fa1a848270652665c30ab905d1c9ea
BrisiaCastrejon <brissmonse@gmail.com>
2018-11-12 15:08:34 -0600

    Pintado de X y O para inicio de estado

--- myCatGame/App.js
+++ myCatGame/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons as Icon } from 'react-native-vector-icons';
 
 export default class App extends React.Component {
+  
   render() {
     return (
       <View style={styles.container}>
 
         <View style={{flexDirection:'row'}}>
-          <View style={[styles.tile, { borderLeftWidth:0, borderTopWidth:0}]} />
-          <View style={[styles.tile, {borderTopWidth:0} ]} />
+          <View style={[styles.tile, { borderLeftWidth:0, borderTopWidth:0}]}>
+            <Icon name='close' style={styles.tileX} /> 
+           </View>
+          <View style={[styles.tile, {borderTopWidth:0} ]}>
+            <Icon name='circle-outline' style={styles.tileO} /> 
+           </View>
           <View style={[styles.tile, {borderTopWidth:0, borderRightWidth:0} ]} />
         </View>
 
@@ -40,5 +46,19 @@ const styles = StyleSheet.create({
     borderWidth:5, 
     width:100,
     height:100,
+  },
+  tileX:{
+    flex:1,
+    color:'red',
+    fontSize:60,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  tileO:{
+    flex:1,
+    color:'green',
+    fontSize:60,
+    alignItems:'center',
+    justifyContent:'center',
   }
 });
